93c45a2593eb98603047dd6a40626b6390cb368d
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-02-09 21:52:28 +0530

    Add useHistory hook

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Search.css';
 import SearchIcon from '@material-ui/icons/SearchOutlined';
 import MicIcon from '@material-ui/icons/Mic';
 import {Button} from '@material-ui/core'
 
-function Search() {
+function Search({btns=false}) {
 
     const [input, setInput] = useState('');
 
-  return <div className='search'>
+    const history = useHistory();
+
+    const search = (e) =>{
+        e.preventDefault();
+        history.push('/search');
+    }
+
+  return <form className='search'>
       <div className='search-box'>
           <SearchIcon className='search-icon'/>
           <input type='text' value={input} onChange={e=> setInput(e.target.value)} />
           <MicIcon className='mic-icon'/>
       </div>
+      {btns?(
       <div className='search-btns'>
-          <Button onClick={Search}>Google Search</Button>
+          <Button type='submit' onClick={search}>Google Search</Button>
           <Button>I'm Feeling Lucky</Button>
-      </div>
+      </div>)
+      :(<></>)}
       
-  </div>;
+  </form>;
 }
 
 export default Search;
